9c05a386c698bdda0c28a06ab28e0907eea99bdc
Ivan Sivolobov <bumstik86@gmail.com>
2025-11-10 17:26:59 +0300

    Done: M1:task3

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -4,4 +4,5 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),
     path('about', views.about),
+    path(f'item/<int:id>/',views.item),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+items = [
+{"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+{"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+{"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+{"id": 7, "name": "Картофель фри" ,"quantity":0},
+{"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
 def home(request):
     author = 'Сиволобов И. В.'
     text = f"""
@@ -24,4 +32,21 @@ def about(request):
         <li>email: <strong>{person['email']}</strong></li>
     </ul>
     """
+    return HttpResponse(text)
+
+def item(request, id):
+    index = None
+    for i in items:
+        if i['id'] == id:
+            index = i
+            text = f"""
+            <h1>Название: {index['name']}</h1>
+            <h1>Количество: {index['quantity']}</h1>
+            """ 
+            break
+        
+    if index is None:
+        text = f"""
+        <h1>Товар с id={id} не найден</h1>
+        """
     return HttpResponse(text)
