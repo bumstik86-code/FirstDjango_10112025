4264933ca2526e45bd50b42d85d070ddcc606332
Ivan Sivolobov <bumstik86@gmail.com>
2025-11-11 13:36:36 +0300

    Done: M2: all

--- MainApp/views.py
+++ MainApp/views.py
@@ -22,39 +22,43 @@ def about(request):
               'surname': 'Иванов',
               'phone_number': '8-923-600-01-02',
               'email': 'vasya@mail.ru'}
-    text = f"""
-    <ul>
-        <li>Имя: <strong>{person['name']}</strong></li>
-        <li>Отчество: <strong>{person['fathername']}</strong></li>
-        <li>Фамилия: <strong>{person['surname']}</strong></li>
-        <li>телефон: <strong>{person['phone_number']}</strong></li>
-        <li>email: <strong>{person['email']}</strong></li>
-    </ul>
-    """
-    return HttpResponse(text)
+    # text = f"""
+    # <ul>
+    #     <li>Имя: <strong>{person['name']}</strong></li>
+    #     <li>Отчество: <strong>{person['fathername']}</strong></li>
+    #     <li>Фамилия: <strong>{person['surname']}</strong></li>
+    #     <li>телефон: <strong>{person['phone_number']}</strong></li>
+    #     <li>email: <strong>{person['email']}</strong></li>
+    # </ul>
+    # """
+    # return HttpResponse(text)
+    return render(request, "about.html", context=person)
 
 def item(request, id):
-    index = None
-    for i in items:
-        if i['id'] == id:
-            index = i
-            text = f"""
-            <h1>Название: {index['name']}</h1>
-            <h1>Количество: {index['quantity']}</h1>
-            """
-            break
+    
+    return render(request, "item.html", context={'goods': items, 'req_id': id})
+#    index = None
+#    for i in items:
+#        if i['id'] == id:
+#            index = i
+#            text = f"""
+#            <h1>Название: {index['name']}</h1>
+#            <h1>Количество: {index['quantity']}</h1>
+#            """
+#            break
+#
+#    if index is None:
+#        text = f"""
+#        <h1>Товар с id={id} не найден</h1>
+#        """
+#    text += '\n<a href="/items">назад к списку товаров</a>'
+#    return HttpResponse(text)
 
-    if index is None:
-        text = f"""
-        <h1>Товар с id={id} не найден</h1>
-        """
-    text += '\n<a href="/items">назад к списку товаров</a>'
-    return HttpResponse(text)
+def items_list(request) -> HttpResponse:
+#    item_list = []
+#    for index, item in enumerate(items, start=1):
+#        item_list.append(f'<li><a href="item/{item['id']}">{item['id']}. {item['name']} (Количество: {item['quantity']})</a></li>')
 
-def items_list(request):
-    item_list = []
-    for index, item in enumerate(items, start=1):
-        item_list.append(f'<li><a href="item/{item['id']}">{item['id']}. {item['name']} (Количество: {item['quantity']})</a></li>')
-
-    result = '<ol>'+'\n'.join(item_list) + '\n</ol>'
-    return HttpResponse(result)
+#    result = '<ol>'+'\n'.join(item_list) + '\n</ol>'
+#    return HttpResponse(result)
+    return render(request, "items.html", context={'goods': items})
